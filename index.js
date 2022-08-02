770fde50448c2ad72373e3e8d5794f11c7fff488
Амаль <50665935+WTTeneger@users.noreply.github.com>
2022-08-03 01:06:12 +0800

    main

--- index.js
+++ index.js
@@ -67,7 +67,10 @@ app.get('*', function (req, res) {
 
 
 
-let db = mongoose.connect('mongodb://0.0.0.0:27017/test-db');
+let db = mongoose.connect('mongodb://127.0.0.1:27017/test-db').then(e => {
+    console.log(e.models);
+})
+
 // Create your Forest Admin agent
 async function startApp() {
     try {
